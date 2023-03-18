0075c28a5a0485c08c20fc1ff5b510b25e1cb7fa
xChicken669 <128184098+xChicken669@users.noreply.github.com>
2023-03-18 19:59:16 +0100

    Add files via upload

---
+++ dog.js
@@ -0,0 +1,31 @@
+function get_random_dog_image(){
+
+    var breed = document.getElementById("myText").value;
+
+    url = `https://dog.ceo/api/breed/${breed}/images/random`
+
+    if (breed == "all") {url = "https://dog.ceo/api/breeds/image/random"}
+
+  
+  
+    fetch(url)
+    .then(function(response){
+      return response.json();
+    })
+    .then(function(data){
+      display_image(data.message);
+    })
+    .catch(function(error){
+      console.log("Error: " + error);
+    });
+  
+  
+  }
+  
+  function display_image(image_url){
+    document.getElementById("image").src = image_url;
+  }
+  
+  
+  
+  
